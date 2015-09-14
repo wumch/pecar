6b9b6851b31db7ddba7636c5fc84f77ec04b22ce
wumuch <wumuch@gmail.com>
2015-09-15 01:43:59 +0800

    build pass

---
+++ app/src/main/jni/Interact.hpp
@@ -0,0 +1,8 @@
+//
+// Created by wumch on 15-9-14.
+//
+
+#ifndef PECAR_INTERACT_HPP
+#define PECAR_INTERACT_HPP
+
+#endif //PECAR_INTERACT_HPP
---
+++ app/src/main/jni/predef.hpp
@@ -0,0 +1,8 @@
+//
+// Created by wumch on 15-9-14.
+//
+
+#ifndef PECAR_PREDEF_HPP
+#define PECAR_PREDEF_HPP
+
+#endif //PECAR_PREDEF_HPP
